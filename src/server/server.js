4e97385deec1d279fade01f56b67cfa5a971c684
Timothy Kachler <kachler@mac.com>
2019-04-14 15:13:33 -0700

    added react site to server

--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require('path');
 require('dotenv').config();
 
 // Configuring server
@@ -12,6 +13,10 @@ const cardController = require('./controllers/cardController.js');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
+
+// Hosting React site
+app.use(express.static(__dirname + '/../../build/'));
+app.get('/', (req, res) => { res.sendFile(path.resolve(__dirname, '../../index.html')); });
 
 // Setting up CRUD routes
 app.get('/cards', cardController.getCards);
